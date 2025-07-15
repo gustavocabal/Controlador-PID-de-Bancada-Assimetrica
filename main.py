3f1b5d34f83319dac71907ddae8b393ecfacecc4
gustavocabal <gustavocabal75@gmail.com>
2025-07-15 00:12:41 -0300

    PID da bancada V1.0

--- main.py
+++ main.py
@@ -9,71 +9,71 @@ cos = math.cos
 
 def pid():
     g = 9.81 # m/s²
-    A = 0.2 # Amplitude
-    f = 0.5 # Hz
-    w = 2*pi*f
+    m1 = 0.5 # kg
+    m2 = 0.7 # kg
+    M = 3 # kg
+    L = 1.5 # metros
+    d = 0.05 # metros
+    L1 = L/2 - d
+    L2 = L/2 + d
+    Ixx = 1/12 * M * L**2 # kg.m²
+    J = Ixx + M*d**2 + m1*L1**2 + m2*L2**2 # Momento de inércia da barra + motores em relação ao eixo x
+    B = 0.2 # Coeficiente de amortecimento em [vou pesquisar a unidade]
 
     # PID
-    kp = -20
-    kd = -10
-    ki = 4 * pow(10, -1)
+    kp = 5
+    kd = 5
+    ki = 0.0001
+    # ziegler nichols
 
-    x0 = 1
+    x0 = -30 # Angulo inicial
     v0 = 0
     z0 = 0
 
-    h = 0.01
+    h = 0.01 # Passo
     T = 20
     N = round(T/h)
     
     t = [i*h for i in range(N+1)]
-    theta = [A * sin(w * ti) for ti in t]
-    eta = [A * sin(w * ti) for ti in t]
 
     x = [x0]    
     v = [v0]
     z = [z0]
     
     for k in range(0, N):
-        tk = t[k]
+        #tk = t[k]
 
         # K1
         k1_z = x[k]
         k1_x = v[k]
-        k1_v = (g + kp * x[k] + kd * v[k] + ki * z[k]) * cos(theta[k]) * cos(eta[k]) - g
+        k1_v = -(kp * x[k] + (kd + B) * v[k] + ki * z[k])/J
 
         # K2
-        theta_k2 = np.interp(tk + h/2, t, theta)
-        eta_k2 = np.interp(tk + h/2, t, eta)
         x_temp2 = x[k] + h/2 * k1_x
         v_temp2 = v[k] + h/2 * k1_v
         z_temp2 = z[k] + h/2 * k1_z
 
         k2_z = x_temp2
         k2_x = v_temp2
-        k2_v = (g + kp * x_temp2 + kd * v_temp2 + ki * z_temp2) * cos(theta_k2) * cos(eta_k2) - g
+        k2_v = -(kp * x_temp2 + (kd + B) * v_temp2 + ki * z_temp2)/J
 
         # K3
-        theta_k3 = np.interp(tk + h/2, t, theta)
-        eta_k3 = np.interp(tk + h/2, t, eta)
         x_temp3 = x[k] + h/2 * k2_x
         v_temp3 = v[k] + h/2 * k2_v
         z_temp3 = z[k] + h/2 * k2_z
 
         k3_z = x_temp3
         k3_x = v_temp3
-        k3_v = (g + kp * x_temp3 + kd * v_temp3 + ki * z_temp3) * cos(theta_k3) * cos(eta_k3) - g
+        k3_v = -(kp * x_temp3 + (kd + B) * v_temp3 + ki * z_temp3)/J
 
         # K4
-        theta_k4 = np.interp(tk + h, t, theta)
-        eta_k4 = np.interp(tk + h, t, eta)
         x_temp4 = x[k] + h * k3_x
         v_temp4 = v[k] + h * k3_v
         z_temp4 = z[k] + h * k3_z
 
         k4_z = x_temp4
         k4_x = v_temp4
-        k4_v = (g + kp * x_temp4 + kd * v_temp4 + ki * z_temp4) * cos(theta_k4) * cos(eta_k4) - g
+        k4_v = -(kp * x_temp4 + (kd + B) * v_temp4 + ki * z_temp4)/J
 
         # Iteração
         z.append(z[k] + h/6 * (k1_z + 2*k2_z + 2*k3_z + k4_z))
@@ -82,8 +82,8 @@ def pid():
 
     plt.plot(t, x)
     plt.xlabel('Tempo (s)')
-    plt.ylabel('Posição x (m)')
-    plt.title('Altura vs tempo')
+    plt.ylabel('Angulo (°)')
+    plt.title('Angulo vs tempo')
     plt.grid(True)
     plt.show()
 
