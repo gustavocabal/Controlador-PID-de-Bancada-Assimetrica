979c58600ec25ef917db6dd7d4083da019824b33
gustavocabal <gustavocabal75@gmail.com>
2025-07-23 01:09:40 -0300

    GRANDES ALTERAÇÕES. V2.0

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sin = math.sin
 cos = math.cos
 
 def pid():
-    #g = 9.81 # m/s²
+    g = 9.81 # m/s²
     m1 = 0.5 # kg
     m2 = 0.7 # kg
     M = 3 # kg
@@ -16,9 +16,15 @@ def pid():
     d = 0.05 # metros
     L1 = L/2 - d
     L2 = L/2 + d
+    L_medio = L/2 # Equivalente à média de L1 e L2
     Ixx = 1/12 * M * L**2 # kg.m²
     J = Ixx + M*d**2 + m1*L1**2 + m2*L2**2 # Momento de inércia da barra + motores em relação ao eixo x
     B = 0.2 # Coeficiente de amortecimento em [vou pesquisar a unidade]
+
+    W = M * g
+    W1 = m1 * g
+    W2 = m2 * g
+    T_Pesos = W * d + W1*L1 - W2*L2
 
     # PID
     kp = 5
@@ -26,9 +32,9 @@ def pid():
     ki = 0.0001
     # ziegler nichols
 
-    x0 = -pi/6 # Angulo inicial em radianos
-    v0 = 0
-    z0 = 0
+    phi0 = -pi/6 # Angulo inicial em radianos
+    dphi0 = 0 # Velocidade angular em rad/s
+    z0 = 0 
 
     h = 0.01 # Passo
     T = 20
@@ -36,55 +42,76 @@ def pid():
 
     t = [i*h for i in range(N+1)]
 
-    x = [x0]    
-    v = [v0]
+    phi = [phi0]    
+    dphi = [dphi0]
     z = [z0]
 
+    torques = [0]
+
+    def Torque(phi_k, dphi_k, z_k):
+        torque = -(kp * phi_k + kd * dphi_k + ki * z_k) * L_medio - cos(phi_k) * T_Pesos - B * dphi_k
+        return torque
+
     for k in range(0, N):
-        #tk = t[k]
 
         # K1
-        k1_z = x[k]
-        k1_x = v[k]
-        k1_v = -(kp * x[k] + (kd + B) * v[k] + ki * z[k])/J
+        k1_z = phi[k]
+        k1_phi = dphi[k]
+        torque_k1 = Torque(phi[k], dphi[k], z[k])
+        k1_dphi = torque_k1 / J
 
         # K2
-        x_temp2 = x[k] + h/2 * k1_x
-        v_temp2 = v[k] + h/2 * k1_v
+        phi_temp2 = phi[k] + h/2 * k1_phi
+        dphi_temp2 = dphi[k] + h/2 * k1_dphi
         z_temp2 = z[k] + h/2 * k1_z
+        torque_k2 = Torque(phi_temp2, dphi_temp2, z_temp2)
 
-        k2_z = x_temp2
-        k2_x = v_temp2
-        k2_v = -(kp * x_temp2 + (kd + B) * v_temp2 + ki * z_temp2)/J
+        k2_z = phi_temp2
+        k2_phi = dphi_temp2
+        k2_dphi = torque_k2 / J
 
         # K3
-        x_temp3 = x[k] + h/2 * k2_x
-        v_temp3 = v[k] + h/2 * k2_v
+        phi_temp3 = phi[k] + h/2 * k2_phi
+        dphi_temp3 = dphi[k] + h/2 * k2_dphi
         z_temp3 = z[k] + h/2 * k2_z
+        torque_k3 = Torque(phi_temp3, dphi_temp3, z_temp3)
 
-        k3_z = x_temp3
-        k3_x = v_temp3
-        k3_v = -(kp * x_temp3 + (kd + B) * v_temp3 + ki * z_temp3)/J
+        k3_z = phi_temp3
+        k3_phi = dphi_temp3
+        k3_dphi = torque_k3 / J
 
         # K4
-        x_temp4 = x[k] + h * k3_x
-        v_temp4 = v[k] + h * k3_v
+        phi_temp4 = phi[k] + h * k3_phi
+        dphi_temp4 = dphi[k] + h * k3_dphi
         z_temp4 = z[k] + h * k3_z
+        torque_k4 = Torque(phi_temp4, dphi_temp4, z_temp4)
 
-        k4_z = x_temp4
-        k4_x = v_temp4
-        k4_v = -(kp * x_temp4 + (kd + B) * v_temp4 + ki * z_temp4)/J
+        k4_z = phi_temp4
+        k4_phi = dphi_temp4
+        k4_dphi = torque_k4 / J
 
         # Iteração
         z.append(z[k] + h/6 * (k1_z + 2*k2_z + 2*k3_z + k4_z))
-        x.append(x[k] + h/6 * (k1_x + 2*k2_x + 2*k3_x + k4_x))
-        v.append(v[k] + h/6 * (k1_v + 2*k2_v + 2*k3_v + k4_v))
+        phi.append(phi[k] + h/6 * (k1_phi + 2*k2_phi + 2*k3_phi + k4_phi))
+        dphi.append(dphi[k] + h/6 * (k1_dphi + 2*k2_dphi + 2*k3_dphi + k4_dphi))
+        torques.append((torque_k1 + 2*torque_k2 + 2*torque_k3 + torque_k4) / 6)
+    
+    Plot_Grafico(t, phi, torques)
 
-    plt.plot(t, x)
-    plt.xlabel('Tempo (s)')
-    plt.ylabel('Angulo (rad)')
-    plt.title('Angulo vs tempo')
-    plt.grid(True)
+def Plot_Grafico(t, phi, torques):
+    fig, axs = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+
+    axs[0].plot(t, phi, color='blue')
+    axs[0].set_ylabel('Ângulo (rad)')
+    axs[0].set_title('Resposta angular e torque aplicado pelo PID')
+    axs[0].grid(True)
+
+    axs[1].plot(t, torques, color='green')
+    axs[1].set_xlabel('Tempo (s)')
+    axs[1].set_ylabel('Torque (N·m)')
+    axs[1].grid(True)
+
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
